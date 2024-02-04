04ef8f165c36f778db0e581c822fc70315b9ca1b
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-04 23:53:09 +0600

    feat: replace demo api to main api

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
-import { featuredProducts } from "../data";
+import getProducts from "../lib/getProducts";
+import { ProductType } from "../types/types";
+// import { featuredProducts } from "../data";
 
-const Featured = () => {
+const Featured = async () => {
+	const featuredProducts: ProductType = await getProducts();
 	return (
 		<div className="w-screen overflow-x-scroll text-red-500">
 			{/* WRAPPER */}
@@ -13,18 +16,18 @@ const Featured = () => {
 						className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-100 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh] group"
 					>
 						{/* IMAGE CONTAINER */}
-						{item.img && (
+						{item?.img && (
 							<div className="relative flex-1 w-full group-hover:rotate-[60deg] transition-all duration-500">
-								<Image src={item.img} alt="" fill className="object-contain" />
+								<Image src={item?.img} alt="" fill className="object-contain" />
 							</div>
 						)}
 						{/* TEXT CONTAINER */}
 						<div className=" flex-1 flex flex-col items-center justify-center text-center gap-4">
 							<h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">
-								{item.title}
+								{item?.title}
 							</h1>
-							<p className="p-4 2xl:p-8">{item.desc}</p>
-							<span className="text-xl font-bold">${item.price}</span>
+							<p className="p-4 2xl:p-8">{item?.desc}</p>
+							<span className="text-xl font-bold">${item?.price}</span>
 							<button className="bg-red-500 text-white p-2 rounded-md">
 								Add to Cart
 							</button>
