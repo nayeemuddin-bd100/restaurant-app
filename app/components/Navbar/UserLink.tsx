beb7126ffed7426a71e6dea440c1abd66d829381
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-06 03:00:24 +0600

    feat: implement next auth login

--- app/components/Navbar/UserLink.tsx
+++ app/components/Navbar/UserLink.tsx
@@ -2,17 +2,21 @@
 
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 const UserLink = () => {
-	const user = false;
 	const { status } = useSession();
+	const handleLogOut = async () => {
+		await signOut();
+		toast.success("Logged out successfully");
+	};
 
 	return (
 		<div>
 			{status === "authenticated" ? (
 				<div>
 					<Link href="/orders">Orders</Link>
-					<span className="ml-4 cursor-pointer " onClick={() => signOut()}>
+					<span className="ml-4 cursor-pointer " onClick={handleLogOut}>
 						Logout
 					</span>
 				</div>
