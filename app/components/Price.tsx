24dd4d6a8dfa4767d20719554a6d2dd470d766c5
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-09 01:56:16 +0600

    fix : price functionality error fix

--- app/components/Price.tsx
+++ app/components/Price.tsx
@@ -1,30 +1,27 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { ProductType } from "../types/types";
 
-type Props = {
-	price: number;
-	id: number;
-	options?: { title: string; additionalPrice: number }[];
-};
-
-const Price = ({ price, id, options }: Props) => {
-	const [total, setTotal] = useState(price);
+const Price = ({ product }: { product: ProductType }) => {
+	const [total, setTotal] = useState(product.price);
 	const [quantity, setQuantity] = useState(1);
 	const [selected, setSelected] = useState(0);
 
 	useEffect(() => {
-		setTotal(
-			quantity * (options ? price + options[selected].additionalPrice : price)
-		);
-	}, [quantity, selected, options, price]);
+		if (product.options?.length) {
+			setTotal(
+				quantity * product.price + product.options[selected].additionalPrice
+			);
+		}
+	}, [quantity, selected, product]);
 
 	return (
 		<div className="flex flex-col gap-4">
-			<h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
+			<h2 className="text-2xl font-bold">${total}</h2>
 			{/* OPTIONS CONTAINER */}
 			<div className="flex gap-4">
-				{options?.map((option, index) => (
+				{product.options?.map((option, index) => (
 					<button
 						key={option.title}
 						className="min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md"
