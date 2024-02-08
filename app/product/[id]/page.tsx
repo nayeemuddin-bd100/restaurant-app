a432d4c0096d2c717242bdcf36c23a1788b36b96
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-09 01:55:33 +0600

    feat: add getProduct api

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,15 +1,20 @@
 import Price from "@/app/components/Price";
-import { singleProduct } from "@/app/data";
+import getSingleProducts from "@/app/lib/getSingleProduct";
 import Image from "next/image";
 
-const SingleProductPage = () => {
+interface IParams {
+	id: string;
+}
+const SingleProductPage = async ({ params }: { params: IParams }) => {
+	const singleProduct = await getSingleProducts(params.id);
+
 	return (
 		<div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center">
 			{/* IMAGE CONTAINER */}
 			{singleProduct.img && (
 				<div className="relative w-full h-1/2 md:h-[70%]">
 					<Image
-						src={singleProduct.img}
+						src={singleProduct?.img}
 						alt=""
 						className="object-contain w-10"
 						fill
@@ -19,14 +24,10 @@ const SingleProductPage = () => {
 			{/* TEXT CONTAINER */}
 			<div className="h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
 				<h1 className="text-3xl font-bold uppercase xl:text-5xl">
-					{singleProduct.title}
+					{singleProduct?.title}
 				</h1>
-				<p>{singleProduct.desc}</p>
-				<Price
-					price={singleProduct.price}
-					id={singleProduct.id}
-					options={singleProduct.options}
-				/>
+				<p>{singleProduct?.desc}</p>
+				<Price product={singleProduct} />
 			</div>
 		</div>
 	);
