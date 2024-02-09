af08e040a300f3adbb0618620da39c42660f75df
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-10 02:51:37 +0600

    feat: add listSkelton for oder page

--- app/components/Skelton/ListSkelton.tsx
+++ app/components/Skelton/ListSkelton.tsx
@@ -1,46 +1,48 @@
 const ListSkelton = () => {
-	// dark:divide-red-700 md:p-6 dark:border-red-700 border-red-200 divide-y divide-red-200
 	return (
-		<div
-			role="status"
-			className="w-[80%] mx-auto p-4 space-y-4  animate-pulse mt-10"
-		>
-			<div className="flex items-center justify-between">
-				<div>
-					<div className="h-2.5 bg-red-300 rounded-full dark:bg-red-600 w-24 mb-2.5"></div>
-					<div className="w-32 h-2 bg-red-200 rounded-full dark:bg-red-700"></div>
-				</div>
-				<div className="h-2.5 bg-red-300 rounded-full dark:bg-red-700 w-12"></div>
-			</div>
-			<div className="flex items-center justify-between pt-4">
-				<div>
-					<div className="h-2.5 bg-red-300 rounded-full dark:bg-red-600 w-24 mb-2.5"></div>
-					<div className="w-32 h-2 bg-red-200 rounded-full dark:bg-red-700"></div>
-				</div>
-				<div className="h-2.5 bg-red-300 rounded-full dark:bg-red-700 w-12"></div>
-			</div>
-			<div className="flex items-center justify-between pt-4">
-				<div>
-					<div className="h-2.5 bg-red-300 rounded-full dark:bg-red-600 w-24 mb-2.5"></div>
-					<div className="w-32 h-2 bg-red-200 rounded-full dark:bg-red-700"></div>
-				</div>
-				<div className="h-2.5 bg-red-300 rounded-full dark:bg-red-700 w-12"></div>
-			</div>
-			<div className="flex items-center justify-between pt-4">
-				<div>
-					<div className="h-2.5 bg-red-300 rounded-full dark:bg-red-600 w-24 mb-2.5"></div>
-					<div className="w-32 h-2 bg-red-200 rounded-full dark:bg-red-700"></div>
-				</div>
-				<div className="h-2.5 bg-red-300 rounded-full dark:bg-red-700 w-12"></div>
-			</div>
-			<div className="flex items-center justify-between pt-4">
-				<div>
-					<div className="h-2.5 bg-red-300 rounded-full dark:bg-red-600 w-24 mb-2.5"></div>
-					<div className="w-32 h-2 bg-red-200 rounded-full dark:bg-red-700"></div>
-				</div>
-				<div className="h-2.5 bg-red-300 rounded-full dark:bg-red-700 w-12"></div>
-			</div>
-			<span className="sr-only">Loading...</span>
+		<div className="p-4 lg:px-20 xl:px-40">
+			<table className="w-full border-separate border-spacing-3">
+				<thead>
+					<tr className="text-left">
+						<th className="hidden md:block">
+							<div className="h-5 bg-gray-300 w-24 animate-pulse"></div>
+						</th>
+						<th>
+							<div className="h-5 bg-gray-300 w-16 animate-pulse"></div>
+						</th>
+						<th>
+							<div className="h-5 bg-gray-300 w-12 animate-pulse"></div>
+						</th>
+						<th className="hidden md:block">
+							<div className="h-5 bg-gray-300 w-32 animate-pulse"></div>
+						</th>
+						<th>
+							<div className="h-5 bg-gray-300 w-12 animate-pulse"></div>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{[...Array(4)].map((_, index) => (
+						<tr className="bg-red-100" key={index}>
+							<td className="hidden md:block py-6 px-1">
+								<div className="h-5 bg-gray-300 w-24 animate-pulse"></div>
+							</td>
+							<td className="py-6 px-1">
+								<div className="h-5 bg-gray-300 w-16 animate-pulse"></div>
+							</td>
+							<td className="py-6 px-1">
+								<div className="h-5 bg-gray-300 w-12 animate-pulse"></div>
+							</td>
+							<td className="hidden md:block py-6 px-1">
+								<div className="h-5 bg-gray-300 w-32 animate-pulse"></div>
+							</td>
+							<td>
+								<div className="h-5 bg-gray-300 w-12 animate-pulse"></div>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</div>
 	);
 };
