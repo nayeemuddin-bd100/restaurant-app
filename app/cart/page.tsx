16a6716de8d7df778bd8cddf8978a7f2fba45793
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-11 16:01:21 +0600

    feat: complete cart functionality

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,13 +2,21 @@
 import Image from "next/image";
 import { useCartStore } from "../utils/store";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const CartPage = () => {
-	const { products, totalItems, totalPrice, removeFromCart } = useCartStore();
+	const { products, totalItems, totalPrice, removeFromCart, resetCart } =
+		useCartStore();
+	const router = useRouter();
 
 	useEffect(() => {
 		useCartStore.persist.rehydrate();
 	}, []);
+
+	const handleClick = () => {
+		resetCart();
+		router.push("/success");
+	};
 
 	return (
 		<div className="h-[calc(100vh - 6rem)] md:h-[calc(100vh - 9rem)] flex flex-col text-red-500 lg:flex-row">
@@ -42,7 +50,7 @@ const CartPage = () => {
 				))}
 			</div>
 			{/* PAYMENT CONTAINER */}
-			<div className="h-1/2 p-4 bg-fuchsia-100 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+			<div className="h-1/2 p-4 bg-fuchsia-100 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-10 xl:px-30 2xl:text-xl 2xl:gap-6">
 				<div className="flex justify-between">
 					<span className="">Subtotal ({totalItems} items)</span>
 					<span className="">{totalPrice}</span>
@@ -60,7 +68,13 @@ const CartPage = () => {
 					<span className="">TOTAL(INCL. VAT)</span>
 					<span className="font-bold">{totalPrice}</span>
 				</div>
-				<button className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end">
+				<button
+					onClick={handleClick}
+					disabled={products.length === 0}
+					className={`${
+						products.length === 0 ? `bg-red-300` : `bg-red-500`
+					} text-white p-3 rounded-md w-1/2 self-end `}
+				>
 					CHECKOUT
 				</button>
 			</div>
