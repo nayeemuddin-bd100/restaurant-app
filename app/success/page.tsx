e41a93e165582833bfdfa65feb64bcca3cec5dfd
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-11 16:02:26 +0600

    feat: add complete payment page

---
+++ app/success/page.tsx
@@ -0,0 +1,16 @@
+const successPage = () => {
+	return (
+		<div className="bg-gray-100 h-[50vh] flex items-center justify-center">
+			<div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md text-center">
+				<div className="text-4xl mb-4">🎉</div>
+				<h1 className="text-2xl text-gray-800 mb-2">Order Successful</h1>
+				<p className="text-gray-600 mb-6">Thank you for your purchase!</p>
+				<button className="bg-red-500 text-white px-4 py-2 rounded-full transition hover:bg-red-600">
+					Continue Shopping
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default successPage;
