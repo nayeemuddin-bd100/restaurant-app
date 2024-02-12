01f22cb7493e201df16334f1b2bb21b8f2b31da3
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-12 23:11:03 +0600

    feat: add menu

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -8,8 +8,7 @@ import CartIcon from "./CartIcon";
 const links = [
 	{ id: 1, title: "Homepage", url: "/" },
 	{ id: 2, title: "Menu", url: "/menu" },
-	{ id: 3, title: "Working Hours", url: "/" },
-	{ id: 4, title: "Contact", url: "/" },
+	{ id: 3, title: "All Items", url: "/all-items" },
 ];
 
 const Menu = () => {
