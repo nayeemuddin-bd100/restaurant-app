240e5055606af8e86d599da0c6fdb90808e45155
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-13 22:34:59 +0600

    feat: all items components

---
+++ app/all-items/page.tsx
@@ -0,0 +1,25 @@
+import Product from "../components/Product";
+import getProducts from "../lib/getProducts";
+import { ProductType } from "../types/types";
+
+const AllItemsPage = async () => {
+	const products: ProductType[] = await getProducts({});
+
+	if (products.length === 0) {
+		return (
+			<h2 className="text-3xl text-red-400 w-screen mt-10 font-semibold text-center mx-auto">
+				Product Not Found
+			</h2>
+		);
+	}
+
+	return (
+		<div className="flex flex-row flex-wrap  text-red-500">
+			{products.map((item) => (
+				<Product key={item.id} item={item} />
+			))}
+		</div>
+	);
+};
+
+export default AllItemsPage;
