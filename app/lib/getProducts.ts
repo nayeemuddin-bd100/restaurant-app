9f44d030d0cedf6cb8b79f4f632bbc2db8f507b3
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-13 22:37:22 +0600

    feat: get products utils

--- app/lib/getProducts.ts
+++ app/lib/getProducts.ts
@@ -1,14 +1,20 @@
-const getProducts = async (category?: string) => {
+const getProducts = async (params: {
+	category?: string;
+	isFeatured?: string;
+}) => {
+	let url;
+	if (params?.category) {
+		url = `http://localhost:3000/api/products?category=${params.category}`;
+	} else if (params?.isFeatured) {
+		url = "http://localhost:3000/api/products?isFeatured=true";
+	} else {
+		url = "http://localhost:3000/api/products";
+	}
 	try {
-		const res = await fetch(
-			`http://localhost:3000/api/products${
-				category ? `?category=${category}` : ""
-			}`,
-			{
-				cache: "force-cache",
-				next: { revalidate: 5 },
-			}
-		);
+		const res = await fetch(url, {
+			cache: "force-cache",
+			next: { revalidate: 5 },
+		});
 
 		if (!res.ok) {
 			throw new Error("Failed to fetch data");
--- app/menu/[category]/page.tsx
+++ app/menu/[category]/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import getProducts from "../../lib/getProducts";
 import { ProductType } from "@/app/types/types";
+import Product from "@/app/components/Product";
 
 interface CategoryProps {
 	params: { category: string };
 }
 const CategoryPage = async ({ params }: CategoryProps) => {
-	const category = params?.category;
-	const products: ProductType[] = await getProducts(category);
+	const products: ProductType[] = await getProducts(params);
 
 	if (products.length === 0) {
 		return (
@@ -21,28 +21,7 @@ const CategoryPage = async ({ params }: CategoryProps) => {
 	return (
 		<div className="flex flex-row flex-wrap  text-red-500">
 			{products.map((item) => (
-				<Link
-					href={`/product/${item.id}`}
-					key={item.id}
-					className="h-[60vh] border-red-500 border flex flex-col items-center justify-around group w-full md:w-1/2 lg:w-1/3 even:bg-fuchsia-100 hover:bg-fuchsia-200 transition-all duration-300"
-				>
-					{/* Image Container */}
-
-					{item.img && (
-						<div className="relative w-full h-[70%]">
-							<Image alt="" src={item.img} fill className="object-contain" />
-						</div>
-					)}
-					{/* Text Container */}
-					<div className="flex items-center justify-between font-bold w-[80%]">
-						<h1 className="text-2xl uppercase p-2">{item.title}</h1>
-						<h2 className="group-hover:hidden text-xl">${item.price}</h2>
-						<button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">
-							{" "}
-							Add to Cart
-						</button>
-					</div>
-				</Link>
+				<Product key={item.id} item={item} />
 			))}
 		</div>
 	);
