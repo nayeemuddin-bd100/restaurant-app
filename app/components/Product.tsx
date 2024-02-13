e8c067004886e50cd5b634db393a366094fb1625
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-13 22:36:26 +0600

    feat: add product component

---
+++ app/components/Product.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { ProductType } from "../types/types";
+import Image from "next/image";
+
+interface ProductProps {
+	item: ProductType;
+}
+const Product = ({ item }: ProductProps) => {
+	return (
+		<Link
+			href={`/product/${item.id}`}
+			key={item.id}
+			className="h-[60vh] border-red-500 border flex flex-col items-center justify-around group w-full md:w-1/2 lg:w-1/3 even:bg-fuchsia-100 hover:bg-fuchsia-200 transition-all duration-300"
+		>
+			{/* Image Container */}
+
+			{item.img && (
+				<div className="relative w-full h-[70%]">
+					<Image alt="" src={item.img} fill className="object-contain" />
+				</div>
+			)}
+			{/* Text Container */}
+			<div className="flex items-center justify-between font-bold w-[80%]">
+				<h1 className="text-2xl uppercase p-2">{item.title}</h1>
+				<h2 className="group-hover:hidden text-xl">${item.price}</h2>
+				<button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">
+					{" "}
+					Add to Cart
+				</button>
+			</div>
+		</Link>
+	);
+};
+
+export default Product;
