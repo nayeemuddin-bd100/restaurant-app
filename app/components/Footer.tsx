4e462c4e8fd51c3d8da5d04b322a18341d9e0cd9
nayeem <nayeemuddin.bd100@gmail.com>
2024-02-16 17:21:24 +0600

    fix typo

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
 	return (
 		<div className="h-15 sm:h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between uppercase text-xs sm:text-base ">
 			<Link href="/" className="font-bold text-xl">
-				BurgerKing
+				PizzaKing
 			</Link>
 			<p>Made with ❤️ by Nayeem</p>
 		</div>
